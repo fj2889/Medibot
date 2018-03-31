53bae98576afc40baa57052691e796770a71c84a
kashimotoxiang <kashimotoxiang@outlook.com>
2018-03-31 22:39:38 +0800

    make word vec
    Former-commit-id: bb4458d146f391af129980af97f14af09a8dddd5
    Former-commit-id: 4ddba10b7c5fdc265cd34f84d112b7a8cf268577

--- init.py
+++ init.py
@@ -1,23 +1,137 @@
 # =================================
 # using for initialize data sets
 # =================================
+# import pandas as pd
 import sample.jieba as jieba
-import sample.insuranceqa_data as insuranceqa
-import logging
+import numpy as np
 import progressbar
+import logging
+import csv
+import multiprocessing as mp
+from math import floor
+from contextlib import contextmanager
+import pickle
 
 
-class dataset():
+class Dataset():
+    # private data
     _stopwordset = ''
     _splitsymbol = ''
 
     _counter = 0
-    _bar = progressbar.ProgressBar(max_value=100)
+    _bar = progressbar.ProgressBar(maxval=100)
+    _raw_data = []
+    _word_data = []
+    _id_data = []
+    # _vec_data = []
+    _data_len = 0
 
-    def __init__(self, *, splitsymbol, stopword_files):
-        if(stopword_files != []):
-            self.set_stopword(stopword_files)
+    _split_scope_sum = 0
+    _split_scope_1 = 0
+    _split_scope_2 = 0
+
+    # public data
+    train_data = []
+    valid_data = []
+    test_data = []
+
+    _id2vec_lookup_list = []
+    _word2id_lookup_list = []
+    # path
+    _userdict = ''
+
+    def __init__(self, *, filename, splitsymbol, word2id_file, id2vec_file, user_dict,  stopword_dict, prop):
+        # ray.init(redis_address="127.0.0.1:35247")
         self._splitsymbol = splitsymbol
+
+        # 初始化停用词表
+        if(stopword_dict != []):
+            self.set_stopword(stopword_dict)
+
+        # 初始化自定义词表
+        if(user_dict != []):
+            self._userdict = user_dict
+
+        # 加载词向量和id查找表
+        _word2id_lookup_list = self.load_obj(word2id_file)
+        _id2vec_lookup_list = np.load(id2vec_file)
+
+        # 导入数据集
+        # 读取csv, 创建dataframe
+        print('导入数据集')
+        with open(filename, "r") as csvFile:
+            reader = csv.reader(csvFile)
+            self._raw_data = [{'index': reader.line_num,
+                               'question': row[0], 'answer': row[1]} for row in reader]
+            csvFile.close()
+        self._data_len = len(self._raw_data)
+
+        # 分词
+        print('\n\n分词')
+        self._word_data = self.split_word(self._raw_data, '')
+
+        # 切分数据集
+        self.split_data_set(prop, self._word_data, self._data_len)
+
+    def split_data_set(self, prop, data, len):
+        """
+        分配数据集
+            :param self:
+            :param prop:
+        """   # split data
+        # split=[]
+        _split_scope_sum = sum(prop)
+        _split_scope_1 = prop[0]/_split_scope_sum
+        _split_scope_2 = prop[1]/_split_scope_sum
+
+        train_data_begin = 0
+        train_data_end = train_data_begin+floor(_split_scope_1*len)
+        valid_data_begin = train_data_end+1
+        valid_data_end = valid_data_begin+floor(_split_scope_2*len)
+        test_data_begin = valid_data_end+1
+        test_data_end = len-1
+
+        self.train_data = data[train_data_begin:train_data_end]
+        self.test_data = data[valid_data_begin:valid_data_end]
+        self.valid_data = data[test_data_begin:test_data_end]
+
+    def split_word(self, data, log_info):
+        # 设置进度条
+        with self.prograssbar(len(data)):
+            return list(self.pool_map(self._split_data, data))
+
+    def _split_data(self, data):
+        """
+        pair question and answer into one setence vector
+            :param question:
+            :param answer:
+            :param negative:
+            :param domain:
+        """
+        self._counter = self._counter + 1
+        self._bar.update(self._counter)
+        # self._bar.update_widgets
+        # split words
+        try:
+            # 分词
+            jieba.load_userdict(self._userdict)
+            seg_question = jieba.lcut(data['question'])
+            seg_answer = jieba.lcut(data['answer'])
+
+            # 去除停用词
+            if (self._stopwordset != []):
+                seg_question = self.movestopwords(seg_question)
+                seg_answer = self.movestopwords(seg_answer)
+
+            # word2id
+            id_question = map(self._word2id_lookup_list.get, seg_question)
+            id_answer = map(self._word2id_lookup_list.get, seg_answer)
+
+            result = [data['index'], seg_question,
+                      seg_answer, id_question, id_answer]
+            return result
+        except Exception as e:
+            print(e)
 
     def set_stopword(self, files):
         """
@@ -38,7 +152,6 @@ class dataset():
         '''
         remove stop words
         '''
-
         try:
             def is_stopwords(word):
                 if (word != '\t'and'\n') and (word not in self._stopwordset):
@@ -73,39 +186,25 @@ class dataset():
 
         return content
 
-    def _vectdata(self, question, answer, negative, domain, splitsymbol):
-        """
-        pair question and answer into one setence vector
-            :param question:
-            :param answer:
-            :param negative:
-            :param domain:
-        """
-        # split words
-        try:
-            seg_question = jieba.lcut(question)
-            seg_answer = jieba.lcut(answer)
+    def save_obj(self, obj, name):
+        with open(name, 'wb') as f:
+            pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-            if (self._stopwordset != []):
-                seg_question = self.movestopwords(seg_question)
-                seg_answer = self.movestopwords(seg_answer)
-            result = [seg_question, seg_answer, negative, domain, splitsymbol]
-            return result
-        except Exception as e:
-            print(e)
+    def load_obj(self, name):
+        with open(name, 'rb') as f:
+            return pickle.load(f)
 
-    def vectdata(self, data):
-        self._counter = self._counter + 1
-        self._bar.update(self._counter)
-        question = data['zh']
-        answer = data['str_answer']
-        negative = data['negatives']
-        domain = data['domain']
-        splitsymbol = self._splitsymbol
-        dataset = self._vectdata(
-            question, answer, negative, domain, splitsymbol)
-        return dataset
-
-    def setbar(self, max):
-        self._bar = progressbar.ProgressBar(max_value=max)
+    @contextmanager
+    def prograssbar(self, maxvalue):
+        self._bar.maxval = maxvalue
+        self._bar.start()
         self._counter = 0
+        yield
+        self._bar.finish()
+
+    def pool_map(self, _map, _data):
+        '''
+        多线程map
+        '''
+        with mp.Pool(processes=(mp.cpu_count() - 1)) as pool:
+            return pool.map(_map, _data)
--- setup.py
+++ setup.py
@@ -1,70 +1,25 @@
 # encoding=utf-8
-import sample.jieba as jieba
-import sample.insuranceqa_data as insuranceqa
+
 import init
-import numpy as np
-# import pandas as pd
-# 这个包快一点
-import ray.dataframe as pd
-from multiprocessing.dummy import Pool as ThreadPool
+
 
 # from sklearn.feature_extraction.text import CountVectorizer
 
+dateset = init.Dataset(filename="corpus/corpus.csv",
+                       splitsymbol='<POS>',
+                       word2id_file='sample/word2vec/id2word.pkl',
+                       id2vec_file='sample/word2vec/vec.npz.npy',
+                       user_dict="dict/自定义词典.txt",
+                       stopword_dict=[
+                           'dict/哈工大停用词表.txt',
+                           #    'dict/中文停用词.txt',
+                           'dict/自定义停用词.txt'],
+                       prop=[0.6, 0.2, 0.2])
 
-dateset = init.dataset(splitsymbol='<POS>', stopword_files=[
-                       'dict/哈工大停用词表.txt', 'dict/中文停用词.txt'])
-
-# 初始化停用词表
-
-
-# 导入数据集(数据格式为字符串)
-print('导入数据集')
-pool_train_data = insuranceqa.load_pool_train()
-pool_test_data = insuranceqa.load_pool_test()
-pool_valid_data = insuranceqa.load_pool_valid()
-pool_answer_data = insuranceqa.load_pool_answers()
-
-
-def pair_answer(question):
-    answerid = question["answers"]
-
-    def id2answer(id):
-        return pool_answer_data[id]['zh']
-    answer = list(map(id2answer, answerid))
-    result = ''.join(answer)
-    question['str_answer'] = result
-    return question
-
-
-# 创建线程池
-print('\n\n训练集分词')
-dateset.setbar(len(pool_train_data))
-pool = ThreadPool(4)
-pd_train_data = pool.map(pair_answer, pool_train_data.values())
-pd_train_data = pool.map(dateset.vectdata, pd_train_data)
-pd_train_data = pd.DataFrame(list(pd_train_data))
-pool.close()
-pool.join()
-
-print('\n\n测试集分词')
-pool = ThreadPool(4)
-dateset.setbar(len(pool_test_data))
-pd_test_data = pool.map(pair_answer, pool_test_data.values())
-pd_test_data = pool.map(dateset.vectdata, pd_test_data)
-pd_test_data = pd.DataFrame(list(pd_test_data))
-pool.close()
-pool.join()
-
-print('\n\nCV集分词')
-pool = ThreadPool(4)
-dateset.setbar(len(pool_valid_data))
-pd_valid_data = pool.map(pair_answer, pool_valid_data.values())
-pd_valid_data = pool.map(dateset.vectdata, pd_valid_data)
-pd_valid_data = pd.DataFrame(list(pd_valid_data))
-pool.close()
-pool.join()
+dateset.save_obj('save.pkl', dateset)
 
 pass
+
 
 # 构建字典
 # count_vect = CountVectorizer()
