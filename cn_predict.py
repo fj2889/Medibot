4b85b05c63fe9338d74a9c5e7db68b674ea60671
fj2889 <1017231735@qq.com>
2018-04-08 18:10:52 +0800

    fixed predict

--- cn_predict.py
+++ cn_predict.py
@@ -13,18 +13,17 @@ from models import model
 from make_data import Dataset
 from models.helpers import load_vocab
 import pickle
-from operator import itemgetter, attrgetter
 
 tf.flags.DEFINE_string("model_dir", "./runs/RNN_CNN_MaxPooling", "Directory to load model checkpoints from")
 tf.flags.DEFINE_string("vocab_processor_file", "./data/vocab_processor.bin", "Saved vocabulary processor file")
 FLAGS = tf.flags.FLAGS
 
 if not FLAGS.model_dir:
-  print("You must specify a model directory")
-  sys.exit(1)
+    print("You must specify a model directory")
+    sys.exit(1)
 
 def tokenizer_fn(iterator):
-  return (x.split(" ") for x in iterator)
+    return (x.split(" ") for x in iterator)
 
 # Load vocabulary
 vp = tf.contrib.learn.preprocessing.VocabularyProcessor.restore(
@@ -36,39 +35,45 @@ with open("data/dataset.pkl","rb") as file:
 
 INPUT_CONTEXT = dataset.raw_data[13]["question"]
 # POTENTIAL_RESPONSES = [dataset.raw_data[i]["answer"] for i in range(len(dataset.raw_data))]
-POTENTIAL_RESPONSES = [dataset.raw_data[i]["answer"] for i in range(20)]
+POTENTIAL_RESPONSES = [dataset.raw_data[i]["answer"] for i in range(400)]
 
-def get_features(context, utterance):
-  context_matrix = np.array(list(vp.transform([context])))
-  utterance_matrix = np.array(list(vp.transform([utterance])))
-  context_len = len(context.split(" "))
-  utterance_len = len(utterance.split(" "))
-  features = {
+def get_features(context, utterances):
+    context_matrix_for_one = next(vp.transform([context])).tolist()
+    print("context_shape  {}".format(len(context_matrix_for_one)))
+    context_matrix = np.array([context_matrix_for_one for i in range(len(utterances))])
+    print("context_matrix_shape{}".format(context_matrix.shape))
+    utterance_matrix = np.array([next(vp.transform([i])).tolist() for i in utterances])
+    print("utterance_matrix_shape{}".format(utterance_matrix.shape))
+    context_len = np.array([len(next(vp._tokenizer([context]))) for i in range(len(utterances))])
+    utterance_len = np.array([len(next(vp._tokenizer([utterances[i]]))) for i in range(len(utterances))])
+    features = {
     "context": tf.convert_to_tensor(context_matrix, dtype=tf.int64),
-    "context_len": tf.constant(context_len, shape=[1,1], dtype=tf.int64),
+    "context_len": tf.constant(context_len, shape=[len(utterances),1], dtype=tf.int64),
     "utterance": tf.convert_to_tensor(utterance_matrix, dtype=tf.int64),
-    "utterance_len": tf.constant(utterance_len, shape=[1,1], dtype=tf.int64),
-  }
-  return features, None
+    "utterance_len": tf.constant(utterance_len, shape=[len(utterances),1], dtype=tf.int64),
+    }
+    return features, None
+
 
 if __name__ == "__main__":
-  hparams = cn_hparams.create_hparams()
-  model_fn = cn_model.create_model_fn(
+    hparams = cn_hparams.create_hparams()
+    model_fn = cn_model.create_model_fn(
                 hparams,
                 model_impl=dual_encoder_model,
                 model_fun=model.RNN_CNN_MaxPooling,
                 RNNInit=tf.nn.rnn_cell.LSTMCell,
                 is_bidirection=True)
-  estimator = tf.contrib.learn.Estimator(model_fn=model_fn, model_dir=FLAGS.model_dir)
+    estimator = tf.contrib.learn.Estimator(model_fn=model_fn, model_dir=FLAGS.model_dir)
 
-  # Ugly hack, seems to be a bug in Tensorflow
-  # estimator.predict doesn't work without this line
-  # estimator._targets_info = tf.contrib.learn.estimators.tensor_signature.TensorSignature(tf.constant(0, shape=[1,1]))
+    # Ugly hack, seems to be a bug in Tensorflow
+    # estimator.predict doesn't work without this line
+    # estimator._targets_info = tf.contrib.learn.estimators.tensor_signature.TensorSignature(tf.constant(0, shape=[1,1]))
 
-  print("Context: {}".format(INPUT_CONTEXT))
-  probs = []
-  for num, r in enumerate(POTENTIAL_RESPONSES):
-    probs.append({num: estimator.predict(input_fn=lambda: get_features(INPUT_CONTEXT, r))})
-  sorted(probs, key=itemgetter(1))
-  for i in probs:
-      print(i)
+    print("Context: {}".format(INPUT_CONTEXT))
+    probs = list(estimator.predict(input_fn=lambda: get_features(INPUT_CONTEXT, POTENTIAL_RESPONSES)))
+    result = {}
+    for num, prob in enumerate(probs):
+        result[num] = prob
+    result = sorted(result.items(), key=lambda asd: asd[1], reverse=True)
+    print("We have found the answer for you:\n{}".format(dataset.raw_data[result[0][0]]["answer"]))
+    print("answer_id: {}".format(result[0][0]))
